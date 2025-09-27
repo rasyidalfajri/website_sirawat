8137b2888d45f78e1ff4dfceaf3a3fce38dedc50
rasyidalfajri <145576902+rasyidalfajri@users.noreply.github.com>
2025-09-27 21:29:05 +0800

    Delete si_rawat directory

--- si_rawat/includes/auth.php
+++
@@ -1,7 +0,0 @@
-<?php
-if (session_status() === PHP_SESSION_NONE) session_start();
-if (!isset($_SESSION['admin'])) {
-    header("Location: /si_rawat/login.php");
-    exit;
-}
-?>
--- si_rawat/schedules/hapus.php
+++
@@ -1,9 +0,0 @@
-<?php
-require __DIR__ . '/../includes/auth.php';
-require __DIR__ . '/../includes/db.php';
-$id = (int)($_GET['id'] ?? 0);
-$stmt = $conn->prepare("DELETE FROM maintenance_schedule WHERE id=?");
-$stmt->bind_param("i", $id);
-$stmt->execute();
-header("Location: index.php?deleted=1");
-exit;
